4095339c67608d21f7036efab83eefd44f6d99cf
Andrei Borodin <124201108+AndreiBorod1n@users.noreply.github.com>
2023-03-06 00:25:33 +0500

    Add files via upload

---
+++ Exercise34.cs
@@ -0,0 +1,14 @@
+﻿// 34. Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] arr = new int[3];
+int value = 0;
+for (int index = 0; index < 3; index++)
+{
+    arr[index] = new Random().Next(100,1000);
+    Console.Write(arr[index]);
+    Console.Write(' ');
+    if(arr[index] % 2 == 0) value++;
+    arr[index] = index + 1;
+}
+Console.WriteLine(value);
+
