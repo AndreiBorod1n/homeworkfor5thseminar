ea56020784244318ff92027897d10f47acc4db5a
Andrei Borodin <andrei.borod1n@mail.ru>
2023-03-06 00:24:30 +0500

    Add exercise36

---
+++ Exercise36.cs
@@ -0,0 +1,18 @@
+﻿// 36. Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32 (Console.ReadLine());
+int[] arr = new int[n];
+int summ = 0;
+Console.Write("Массив: ");
+for (int index = 0; index < n; index++)
+{
+    arr[index] = new Random().Next(0,100);
+    Console.Write(arr[index]);
+    Console.Write(' ');
+    if(index % 2 != 0) summ = summ + arr[index];
+    arr[index] = index + 1;
+}
+Console.WriteLine(' ');
+Console.Write($"Сумма чисел нечетных элементов равна: {summ}");
+
+
